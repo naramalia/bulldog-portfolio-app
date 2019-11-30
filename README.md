cdacad2046019cd1daa6e1c23ad963c550023b48
Nara Cardenas <naramalia@Naras-MacBook-Pro.local>
2019-11-29 17:17:40 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# BullDog webpage Project
+
+> One of my projects as I work through [Bottega Code School](https://bottega.tech/)
+
+*A work in progress*
---
+++ src/components/app.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className='app'>
+        <h1>BullDog</h1>
+        <h2>Actor : Writer : Designer : Director</h2>
+      </div>
+    );
+  }
+}
